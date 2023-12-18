adabe55c31271d646f2f37187abd8b75ad66edbc
Gustavo Cunha <gustavoparreira@hotmail.com>
2023-12-18 17:30:50 -0300

    mudanças na aula 14

--- aula14.py
+++ aula14.py
@@ -2,8 +2,13 @@ a = 'AAAAA'
 b = 'BBBBBB'
 c = 1.1
 string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
-formato = string.format(
-    nome1=a, nome2=b, nome3=c
-)
-
+string_ini = 'a={} b={} c={}'
+formato = string_ini.format(a, b, c)
+#tudo em python é um objeto, e objetos podem executar ações. Essas ações são chamadas de métodos. 
+#
 print(formato)
+
+#formato = string.format(
+#    nome1=a, nome2=b, nome3=c
+#)
+
