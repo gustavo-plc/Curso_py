e30856d92f0e0d8e2538ae0958b00fc7e53df758
Gustavo Cunha <gustavoparreira@hotmail.com>
2023-12-18 18:37:40 -0300

    initial commit

--- aula14.py
+++ aula14.py
@@ -1,14 +1,29 @@
+#formatação de string com o método format
+
 a = 'AAAAA'
 b = 'BBBBBB'
 c = 1.1
 string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
-string_ini = 'a={} b={} c={}'
+string_ini = 'a={0} b={1} c={2:.2f}'
 formato = string_ini.format(a, b, c)
 #tudo em python é um objeto, e objetos podem executar ações. Essas ações são chamadas de métodos. 
 #
 print(formato)
 
-#formato = string.format(
-#    nome1=a, nome2=b, nome3=c
-#)
+formato1 = string.format(
+    nome1=a, nome2=b, nome3=c
+)
+
+print(formato1) 
+
+#parâmetro nomeado é quando se dá nome às variáveis dentro da chamada das funções
+#dentro do parenteses na chamada da função, os argumentos são os valores de cada variável assume, e os 
+#parâmetros são as variáveis em si.
+
+#quando uma função está dentro de um objeto, essa função é chamada de método.
+
+#nome = "Luiz"
+#idade = 23
+#formato = '{n} tem {i} anos'
+#print(formato.format(n=nome, i=idade))
 
