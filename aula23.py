7125d3c7525af20076619143ce3f8917be9043a7
Gustavo Cunha <gustavoparreira@hotmail.com>
2023-12-30 17:23:46 -0300

    operador not

---
+++ aula23.py
@@ -0,0 +1,15 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+senha = input('Senha: ')
+
+
+if not senha:
+    print('Vc não digitou nada')
+
+print(not True)  # False
+print(not False)  # True
+
+
