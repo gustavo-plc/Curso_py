b29854f10bee469bca565b2b47fd0bc0d899ed27
Gustavo Cunha <gustavoparreira@hotmail.com>
2023-12-30 14:19:49 -0300

    função input

--- aula15.py
+++ aula15.py
@@ -1,1 +1,16 @@
 #testando nova branch
+
+#input('Qual o seu nome? ') #input só exibe o texto que está entre aspas
+
+#agora armazena: 
+
+nome = input('Qual o seu nome? ')
+print(f'O seu nome é {nome}!')
+
+#a função input retorna sempre uma string
+
+nmr_1 = input('digite um número:')
+nmr_2 = input('Digite outro número')
+
+print(f'A soma dos dois números é: {nmr_1+nmr_2}')
+
